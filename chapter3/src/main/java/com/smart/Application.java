12d29142838e34c7c64928a540e330a3ac471243
wanggang <wanggang@ly-sky.com>
2017-07-25 16:39:09 +0800

    3.3

---
+++ chapter3/src/main/java/com/smart/Application.java
@@ -0,0 +1,14 @@
+package com.smart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Administrator on 7/25 0025.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
